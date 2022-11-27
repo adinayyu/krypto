9d9d965ba0752b22515d0ade3cfced3d3ba9a0e3
adinayyu <adinayyu@gmail.com>
2022-11-27 19:02:26 +0530

    First Commit

---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const APIKEY = import.meta.env.VITE_GIPHY_API;
+
+const useFetch = ({ keyword }) => {
+  const [gifUrl, setGifUrl] = useState("");
+
+  const fetchGifs = async () => {
+    try {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(" ").join("")}&limit=1`);
+      const { data } = await response.json();
+
+      setGifUrl(data[0]?.images?.downsized_medium.url);
+    } catch (error) {
+      setGifUrl("https://colinbendell.cloudinary.com/image/upload/c_crop,f_auto,g_auto,h_350,w_400/v1512090971/Wizard-Clap-by-Markus-Magnusson.gif");
+    }
+  };
+
+  useEffect(() => {
+    if (keyword) fetchGifs();
+  }, [keyword]);
+
+  return gifUrl;
+};
+
+export default useFetch;
